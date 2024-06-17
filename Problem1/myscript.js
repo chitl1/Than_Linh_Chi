38030a2b766fd3f429295a484600e372097b6ed1
Chi <thanlinhchi010995.dhcn@gmail.com>
2024-06-17 16:02:58 +0700

    problem1

---
+++ Problem1/myscript.js
@@ -0,0 +1,16 @@
+const sum_to_n_a = (n) => {
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
+};
+
+const sum_to_n_b = (n) => {
+  return (n * (n + 1)) / 2;
+};
+
+const sum_to_n_c = (n) => {
+  if (n <= 1) return n;
+  return n + sum_to_n_c(n - 1);
+};
